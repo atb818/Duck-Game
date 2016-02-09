2803476f70481a6b6f62f4cdddf5a9ca453db5a6
Orien Longo <longo788@newschool.edu>
2016-02-08 19:49:55 -0500

    Prefab

---
+++ HeadRotationTest/Assets/Rotate/HeadRotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadRotate : MonoBehaviour {
+
+    Vector3 mousePos;
+    float playerRotationAngle;
+    public GameObject targetPlayer;
+    Vector3 objectPos;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //mousePos.x = mousePos.x - objectPos.x;
+        //mousePos.y = mousePos.y - objectPos.z;
+        //playerRotationAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0, playerRotationAngle, 0));
+    }
+}

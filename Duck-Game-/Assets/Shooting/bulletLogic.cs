8b17cc462d426db0752867236855c51ed9fe6f1e
Danny <shortieofcheese@gmail.com>
2016-02-10 02:10:39 -0500

    Prototype of first shooting mechanic

---
+++ Duck-Game-/Assets/Shooting/bulletLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletLogic : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public int speed;
+    int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * speed);
+    }
+
+    void Update ()
+    {
+        if (count == 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        count++;
+    }
+}
---
+++ Duck-Game-/Assets/Shooting/shootBullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootBullet : MonoBehaviour {
+
+
+    public GameObject bullet;
+    public GameObject boy;
+	
+
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+            Instantiate(bullet, transform.position, boy.transform.rotation);
+
+    }
+}

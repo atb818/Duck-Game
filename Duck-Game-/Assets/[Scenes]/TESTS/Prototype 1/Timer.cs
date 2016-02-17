6e4d2e5a2b96f057b203d697605851f6ef46d3b7
Danny <shortieofcheese@gmail.com>
2016-02-17 10:28:52 -0500

    Timer and collision and sounds

---
+++ Duck-Game-/Assets/[Scenes]/TESTS/Prototype 1/Timer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    int time = 6;
+
+    public GameObject words;
+    Text timeRemain;
+
+    public AudioSource bell;
+
+    bool soundPlayed;
+
+    void Start () {
+        soundPlayed = false;
+        InvokeRepeating("Countdown", 0f, 1f);
+        timeRemain = words.GetComponent<Text>();
+    }
+	
+	
+	void Update () {
+        timeRemain.text = " " + time;
+        if(time == 0)
+        {
+            if (!soundPlayed)
+            {
+                bell.Play();
+                soundPlayed = true;
+            }
+            CancelInvoke();
+        }
+    }
+
+    void Countdown()
+    {
+        time--;
+    }
+}

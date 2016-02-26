a45aaef0a01c0f7e5e4907a0e63a1a7bc9ed9e3c
Danny <shortieofcheese@gmail.com>
2016-02-25 20:33:16 -0500

    Bread and trails

---
+++ Duck-Game-/Assets/projector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projector : MonoBehaviour {
+
+
+
+    public float power = 10.0F;
+
+    Vector3 StartPos;
+
+    void Start() {
+        StartPos = GetComponent<Transform>().position;
+    }
+
+	void FixedUpdate () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            power += Time.deltaTime;
+            GetComponent<Rigidbody>().AddForce(transform.forward * power * 10);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            transform.position = StartPos;
+        }
+
+    }
+
+
+}

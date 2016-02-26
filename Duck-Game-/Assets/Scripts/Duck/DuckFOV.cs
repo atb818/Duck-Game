ddba85d922bde7d727c26bb362773c5e701dcca3
Andrew Benson <andros818@gmail.com>
2016-02-25 20:35:40 -0500

    began new duck logic

---
+++ Duck-Game-/Assets/Scripts/Duck/DuckFOV.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckFOV : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collision player){
+		if (player.CompareTag("Player")){
+			//GET IT
+		}
+	}
+}

0831b955703f55e27189c638adef3642e145c3a2
Danny <shortieofcheese@gmail.com>
2016-02-29 20:26:01 -0500

    Made bread UI

---
+++ Duck-Game-/Assets/ammoCheck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using UnityEngine.UI;
+
+public class ammoCheck : MonoBehaviour {
+
+    Text ammoLeft;
+
+	void Start () {
+        ammoLeft = GetComponent<Text>();
+	}
+	
+	
+	void Update () {
+        ammoLeft.text = "" + shootBullet.ammo;
+	
+	}
+}
+

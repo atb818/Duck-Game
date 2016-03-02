de864879cf95b80c3e2deb41d451f957caafa4d4
Andrew Benson <andros818@gmail.com>
2016-03-02 14:05:04 -0500

    improved duck AI

--- Duck-Game-/Assets/Scripts/Duck/DuckFOV.cs
+++ Duck-Game-/Assets/Scripts/Duck/DuckFOV.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class DuckFOV : MonoBehaviour {
 
 	void OnTriggerEnter(Collider player){
 		if (player.CompareTag("Player")){
+			SceneManager.LoadScene(0);
 			print("SPOTTED");
 		}
 	}

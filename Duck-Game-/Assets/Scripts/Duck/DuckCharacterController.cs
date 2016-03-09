72b622d8d1af9b6adcd2317999d62f19941f9fcb
Andrew Benson <andros818@gmail.com>
2016-03-09 12:12:18 -0500

    Updated duck logic-- sees player based on distance and angle

--- Duck-Game-/Assets/Scripts/Duck/DuckCharacterController.cs
+++ Duck-Game-/Assets/Scripts/Duck/DuckCharacterController.cs
@@ -16,9 +16,13 @@ public class DuckCharacterController : MonoBehaviour {
 	bool isResting = false;
 
 	//SEE PLAYER
+	//Distance
 	float playerDist;
 	public float fovDist;
 	bool playerInDist = false;
+	//Angle
+	float playerAngle;
+	public float fovAngle;
 	bool playerInAngle = false;
 	public GameObject FOV;
 
@@ -34,22 +38,15 @@ public class DuckCharacterController : MonoBehaviour {
 		transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
 
 		playerDist = Vector3.Distance(DB.transform.position, transform.position);
+		Vector3 playerDir = DB.transform.position - transform.position;
+		Vector3 duckDir = transform.forward;
+		playerAngle = Vector3.Angle(playerDir, duckDir);
 
-		if (isResting == false){
-			
-			if (playerDist <= fovDist){
-				playerInDist = true;
-				print ("duck sees player");
-			}
-
-			if (playerInDist){
-    			transform.LookAt(DB.transform);
-    			cc.Move(transform.forward * Time.deltaTime * 2	);
-			}
-		}
+		GetPlayer();
 
 		if (target != null){
 			playerInDist = false;
+			playerInAngle = false;
 			breadDist = Vector3.Distance(target.transform.position, transform.position);
 			if (target.CompareTag("Bread")){
 				if (getBread && !isResting){
@@ -66,6 +63,25 @@ public class DuckCharacterController : MonoBehaviour {
 		//NEW BREAD SCRIPT
 
 	
+	}
+
+	void GetPlayer () {
+		if (isResting == false){
+			//Get distance to player
+			if (playerDist <= fovDist){
+				playerInDist = true;
+				//print ("duck sees player");
+			}
+			//Get angle to player
+			if (playerAngle <= fovAngle){
+				playerInAngle = true;
+			}
+
+			if (playerInDist && playerInAngle){
+				transform.LookAt(DB.transform);
+				cc.Move(transform.forward * Time.deltaTime * 2	);
+			}
+		}
 	}
 
 	public void EatBread (GameObject bread){

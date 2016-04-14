56a15416a484f39abc932d13332cb1152f8eff80
Andrew <andros818@gmail.com>
2016-04-13 21:22:42 -0400

    'fire' by front door, lobby doors close, delay start to 1st duck, started building gym

---
+++ Duck-Game-/Assets/Scripts/Duck/Activate1stDuck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activate1stDuck : MonoBehaviour {
+
+	public GameObject duck1;
+
+	void Start () {
+		duck1.SetActive(false);
+	}
+	
+	void OnTriggerEnter (Collider db) {
+		if (db.CompareTag("Player")){
+			duck1.SetActive(true);
+		}
+	}
+}

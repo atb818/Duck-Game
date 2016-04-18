8efc693888ced2596e8a730b4e6e8a75ed34fd67
Andrew <andros818@gmail.com>
2016-04-18 11:55:59 -0400

    created breakable basketball rack

---
+++ Duck-Game-/Assets/Scripts/Environment/BBallRack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BBallRack : MonoBehaviour {
+
+	public GameObject[] parts;
+
+	void OnTriggerEnter (Collider other){
+		if (other.CompareTag("Player") || other.CompareTag("Bread")){
+			foreach (GameObject part in parts) {
+				part.GetComponent<Rigidbody>().isKinematic = false;
+        	}
+		}
+	}
+}

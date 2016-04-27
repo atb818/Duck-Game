9f60079d4d237fdfbefb6c1499d6aa04cc74f0c8
Andrew Benson <Andrew Benson>
2016-04-27 18:04:36 -0400

    change change chaaaaange

---
+++ Duck-Game-/Assets/Scripts/Environment/Bush/InBush.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InBush : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider player){
+		if (player.CompareTag("Player")){
+			goInside.insideLockerGlobal = true;
+		}
+	}
+
+	void OnTriggerExit (Collider player){
+		if (player.CompareTag("Player")){
+			goInside.insideLockerGlobal = false;
+		}
+	}
+}

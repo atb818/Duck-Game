3b05ca238a33fdaa6a546f3839a16aa5ecd409ab
Orien Longo <longo788@newschool.edu>
2016-04-29 03:21:41 -0400

    tried to fix the scripts to see if it would change the duck animations getting stuck

--- Duck-Game-/Assets/Scripts/Duck/DuckAnimScript.cs
+++ Duck-Game-/Assets/Scripts/Duck/DuckAnimScript.cs
@@ -21,19 +21,26 @@ public class DuckAnimScript : MonoBehaviour
     {
         duckVel = Dcontroller.velocity/Time.deltaTime;
 
-        if (duckVel.magnitude < .25f)
+        if (duckVel.magnitude < 1f)
         {
             duckAnim.SetBool("IsMoving", false);
+            duckAnim.SetFloat("AnimSpeed", 1f);
         }
         else
         {
             duckAnim.SetBool("IsMoving", true);
-            if (duckVel.magnitude < 3f)
+            if (duckVel.magnitude >= 2f && duckVel.magnitude < 3f)
             {
                 duckAnim.SetFloat("AnimSpeed", 2.5f);
             }
-            else {
+            if (duckVel.magnitude > 3f)
+            {
                 duckAnim.SetFloat("AnimSpeed", 3.5f);
+            }
+            else if (duckVel.magnitude < 1f)
+            {
+                duckAnim.SetBool("IsMoving", false);
+                duckAnim.SetFloat("AnimSpeed", 1f);
             }
         }
     }

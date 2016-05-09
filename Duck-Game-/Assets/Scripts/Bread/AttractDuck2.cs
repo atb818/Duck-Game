1cfdc2e127efef1a45247858f2fc6218455c8a66
Orien Longo <longo788@newschool.edu>
2016-05-09 17:52:37 -0400

    Changed AttractDuck2.cs

--- Duck-Game-/Assets/Scripts/Bread/AttractDuck2.cs
+++ Duck-Game-/Assets/Scripts/Bread/AttractDuck2.cs
@@ -6,10 +6,33 @@ public class AttractDuck2 : MonoBehaviour {
 	//Trigger turned on via AttractDuck.cs
 
 	public GameObject bread;
+    Vector3 _bread, _duck;
+    float proximity;
 
-	void OnTriggerEnter(Collider duck){
+    void OnTriggerEnter(Collider duck)
+    {
 		if (duck.CompareTag("Duck")){
+            _bread = bread.GetComponent<Vector3>();
+            _duck = duck.GetComponent<Vector3>();
 			duck.GetComponent<DuckCharacterController>().EatBread(bread.gameObject);
+
 		}
 	}
+
+
+
+    void FixedUpdate()
+    {
+        proximity = Vector3.Distance(_bread, _duck);
+        if (proximity < 1f)
+        {
+            StartCoroutine("BreadLife", 8f * Time.deltaTime);
+        }
+    }
+
+    IEnumerator BreadLife(float eatTime)
+    {
+        yield return new WaitForSeconds(eatTime);
+        DestroyObject(this);
+    }
 }
